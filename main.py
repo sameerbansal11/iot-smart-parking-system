e6ef8946c3bed2b4cac391c6c44f5e139074b6f4
Sameer Bansal <sameerbansal2005@gmail.com>
2026-05-03 09:30:20 +0530

    feat: Add initial project structure and main script

---
+++ main.py
@@ -0,0 +1,11 @@
+# iot-smart-parking-system
+# Author: Sameer Bansal
+# SRM Institute of Science and Technology
+
+def main():
+    print("IoT Smart Parking System Simulation in Python")
+    print("Project: iot-smart-parking-system")
+    print("Author: Sameer Bansal | RA2311032010061")
+
+if __name__ == "__main__":
+    main()
